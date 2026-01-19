6b451dc758392dd57687c745cdf9194329f00e5d
SAURABH YADAV <saurabhsj069@gmail.com>
2026-01-19 23:55:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-class Person {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
-  }
-
-  greet() {
-    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
-  }
-}
-
-class Employee extends Person {
-  constructor(name, age, jobTitle) {
-    super(name, age);
-    this.jobTitle = jobTitle;
-  }
-
-  jobGreet() {
-    console.log(
-      `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
-    );
-  }
-}
 const person = new Person("Alice", 25);
 person.greet();
 // Hello, my name is Alice, I am 25 years old.
